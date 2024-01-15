24eaaa25eb18b8ff0ddced046edc003edc6fed42
ibrohim <ibroxim.rashidov@yandex.ru>
2024-01-15 17:56:59 +0500

    sdfds

--- config/settings.py
+++ config/settings.py
@@ -75,16 +75,17 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'library',
-        'USER': 'library_admin',
-        'PASSWORD': 'library_admin',
-        'HOST': 'localhost',
-        'PORT': 5432,
-    }
-}
+
+
+
+DATABASES = { 
+    "default": { 
+        "ENGINE": "django.db.backends.sqlite3", 
+        "NAME": BASE_DIR / "db.sqlite3", 
+    } 
+} 
+
+
 AUTH_USER_MODEL = 'main.Employee'
 
 # Password validation
